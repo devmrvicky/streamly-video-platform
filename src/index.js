39d8bff76ef6048df1383db372794a4c00475fb1
Vicky K <devmrvicky@gmail.com>
2023-12-07 17:53:15 +0530

    add util file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require("dotenv").config({ path: "./env" });
 
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -7,7 +8,13 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.log("mongo db connection failed !!! ", error));
 
 /* 
 import express from "express";
