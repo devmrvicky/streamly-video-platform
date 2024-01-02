fea472e18835e54a947bb2ce4a42590e3f2afbf4
Vicky K <devmrvicky@gmail.com>
2024-01-02 10:20:44 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// import router
+import userRouter from "./routes/user.routes.js";
+
+// route declaration
+app.use("/api/v1/users", userRouter);
+
+// https://localhost:8000/api/v1/users/register
+
 export { app };
